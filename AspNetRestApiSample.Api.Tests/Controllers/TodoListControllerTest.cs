439619a4385735152163224059867c7dc180775f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-07-28 11:19:08 +0300

    add contr tests

--- AspNetRestApiSample.Api.Tests/Controllers/TodoListControllerTest.cs
+++ AspNetRestApiSample.Api.Tests/Controllers/TodoListControllerTest.cs
@@ -1,25 +1,62 @@
-﻿using AspNetRestApiSample.Api.Controllers;
-using AspNetRestApiSample.Api.Services;
-using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
 
 namespace AspNetRestApiSample.Api.Tests.Controllers
 {
+  using Moq;
+
+  using AspNetRestApiSample.Api.Controllers;
+  using AspNetRestApiSample.Api.Dtos;
+  using AspNetRestApiSample.Api.Services;
+
   [TestClass]
   public sealed class TodoListControllerTest
   {
-    private Mock<ITodoListService>
+    private Mock<ITodoListService> _todoListServiceMock;
 
-    private TodoListController _controller;
+    private TodoListController _todoListController;
 
     [TestInitialize]
     public void Initialize()
     {
+      _todoListServiceMock = new Mock<ITodoListService>();
+      _todoListController = new TodoListController(_todoListServiceMock.Object);
+    }
 
+    [TestMethod]
+    public async Task GetTodoListTest()
+    {
+      await _todoListController.GetTodoList(
+        new GetTodoListRequestDto(), CancellationToken.None);
+    }
+
+    [TestMethod]
+    public async Task SearchTodoListsTest()
+    {
+      await _todoListController.SearchTodoLists(
+        new SearchTodoListsRequestDto(), CancellationToken.None);
+    }
+
+    [TestMethod]
+    public async Task AddTodoListTest()
+    {
+      await _todoListController.AddTodoList(
+        new AddTodoListRequestDto(), CancellationToken.None);
+    }
+
+    [TestMethod]
+    public async Task UpdateTodoListTest()
+    {
+      await _todoListController.UpdateTodoList(
+        new UpdateTodoListRequestDto(), CancellationToken.None);
+    }
+
+    [TestMethod]
+    public async Task DeleteTodoListTest()
+    {
+      await _todoListController.DeleteTodoList(
+        new DeleteTodoListRequestDto(), CancellationToken.None);
     }
   }
 }
