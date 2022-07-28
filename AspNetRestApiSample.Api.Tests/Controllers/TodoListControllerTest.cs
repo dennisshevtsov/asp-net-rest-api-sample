8f212e04fe75573cbcedfd60ea4c3f208b029040
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-07-28 11:02:28 +0300

    add contr tets

---
+++ AspNetRestApiSample.Api.Tests/Controllers/TodoListControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using AspNetRestApiSample.Api.Controllers;
+using AspNetRestApiSample.Api.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetRestApiSample.Api.Tests.Controllers
+{
+  [TestClass]
+  public sealed class TodoListControllerTest
+  {
+    private Mock<ITodoListService>
+
+    private TodoListController _controller;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+
+    }
+  }
+}
---
+++ AspNetRestApiSample.Api.Tests/Services/TodoListServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetRestApiSample.Api.Tests.Services
+{
+  [TestClass]
+  public sealed class TodoListServiceTest
+  {
+  }
+}
