025010d004fbd61f56d6e2eb47202c4b346e7452
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-07-30 11:09:51 +0300

    add comments for dbcontext

--- AspNetRestApiSample.Api/Storage/AspNetRestApiSampleDbContext.cs
+++ AspNetRestApiSample.Api/Storage/AspNetRestApiSampleDbContext.cs
@@ -8,8 +8,11 @@ namespace AspNetRestApiSample.Api.Storage
 
   using AspNetRestApiSample.Api.Configurations;
 
+  /// <summary>Represents a session with the database and can be used to query and save instances of your entities.</summary>
   public sealed class AspNetRestApiSampleDbContext : DbContext
   {
+    /// <summary>Configure the model that was discovered by convention from the entity types.</summary>
+    /// <param name="modelBuilder">Provides a simple API surface for configuring a <see cref="IMutableModel" /> that defines the shape of your entities, the relationships between them, and how they map to the database.</param>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
       modelBuilder.ApplyConfiguration(new TodoListEntityTypeConfiguration());
