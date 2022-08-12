e87d5774e8100611f2e91a54477f2f04e33b5b5a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-08-12 19:42:53 +0300

    clean code

--- AspNetRestApiSample.Api/Dtos/GetTodoListTaskResponseDto.cs
+++ AspNetRestApiSample.Api/Dtos/GetTodoListTaskResponseDto.cs
@@ -1,7 +1,11 @@
-﻿using AspNetRestApiSample.Api.Indentities;
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
 
 namespace AspNetRestApiSample.Api.Dtos
 {
+  using AspNetRestApiSample.Api.Indentities;
+
   /// <summary>Represents data of a response for the get TODO list query.</summary>
   public sealed class GetTodoListTaskResponseDto : ITodoListIdentity, ITodoListTaskIdentity
   {
