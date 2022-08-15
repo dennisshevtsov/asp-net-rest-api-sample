007943791031534aaa2d4d9ed2b48a1711357b34
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-08-15 20:04:32 +0300

    start repo

---
+++ AspNetRestApiSample.Api/Storage/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using AspNetRestApiSample.Api.Entities;
+
+namespace AspNetRestApiSample.Api.Storage
+{
+  public interface IRepository<TEntity> where TEntity : TodoListEntityBase
+  {
+    public TEntity Create(object command);
+
+    public void Update(TEntity entity);
+
+    public void Delete(TEntity entity);
+  }
+}
