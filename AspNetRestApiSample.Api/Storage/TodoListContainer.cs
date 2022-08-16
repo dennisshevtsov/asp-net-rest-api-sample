1fb137fc1925417ba57bbbf6f81c94505d2cf407
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-08-16 11:01:07 +0300

    impl containers

---
+++ AspNetRestApiSample.Api/Storage/TodoListContainer.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetRestApiSample.Api.Storage
+{
+  using Microsoft.EntityFrameworkCore;
+
+  using AspNetRestApiSample.Api.Entities;
+
+  public sealed class TodoListContainer : ContainerBase<TodoListEntity>, ITodoListContainer
+  {
+    public TodoListContainer(DbContext dbContext) : base(dbContext)
+    {
+    }
+  }
+}
