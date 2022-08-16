ddbe4b5263965a24f2b52262b1dac54832df8edb
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-08-16 10:58:57 +0300

    add todo list and todo list task containers

---
+++ AspNetRestApiSample.Api/Storage/IContainer.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetRestApiSample.Api.Storage
+{
+  using AspNetRestApiSample.Api.Entities;
+
+  public interface IContainer<TEntity> where TEntity : TodoListEntityBase
+  {
+    public Task<TEntity> GetAttachedEntityAsync(Guid id, Guid partitionKey);
+
+    public Task<TEntity> GetDetachedEntityAsync(Guid id, Guid partitionKey);
+
+    public TEntity Create(object command);
+
+    public void Update(TEntity entity);
+
+    public void Delete(TEntity entity);
+  }
+}
