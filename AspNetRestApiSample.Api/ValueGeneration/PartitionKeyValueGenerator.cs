4575003c5af7da8e6f2f55b8846d6a187e751a1e
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-08-27 13:57:16 +0300

    clean generators

--- AspNetRestApiSample.Api/ValueGeneration/PartitionKeyValueGenerator.cs
+++ AspNetRestApiSample.Api/ValueGeneration/PartitionKeyValueGenerator.cs
@@ -9,10 +9,15 @@ namespace AspNetRestApiSample.Api.ValueGeneration
 
   using AspNetRestApiSample.Api.Entities;
 
+  /// <summary>Generates values for properties when an entity is added to a context.</summary>
   public sealed class PartitionKeyValueGenerator : ValueGenerator
   {
+    /// <summary>Gets a value indicating whether the values generated are temporary or are permanent.</summary>
     public override bool GeneratesTemporaryValues => false;
 
+    /// <summary>Gets a value to be assigned to a property.</summary>
+    /// <param name="entry">The change tracking entry of the entity for which the value is being generated.</param>
+    /// <returns>The generated value.</returns>
     protected override object? NextValue(EntityEntry entry)
     {
       if (entry.Entity is TodoListEntity todoListEntity)
@@ -27,7 +32,8 @@ namespace AspNetRestApiSample.Api.ValueGeneration
           return todoListTaskEntity.TodoListId;
         }
 
-        if (todoListTaskEntity.TodoList != null)
+        if (todoListTaskEntity.TodoList != null &&
+            todoListTaskEntity.TodoList.Id != default)
         {
           return todoListTaskEntity.TodoList.Id;
         }
