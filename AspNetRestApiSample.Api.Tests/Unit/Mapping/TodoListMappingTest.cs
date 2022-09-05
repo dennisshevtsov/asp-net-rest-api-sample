52b04d522ff4514791611a0de62c25055b6bb34b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-09-05 18:29:46 +0300

    start mapping test

---
+++ AspNetRestApiSample.Api.Tests/Unit/Mapping/TodoListMappingTest.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetRestApiSample.Api.Tests.Unit.Mapping
+{
+  using AutoMapper;
+  using Microsoft.Extensions.DependencyInjection;
+
+  [TestClass]
+  public sealed class TodoListMappingTest
+  {
+#pragma warning disable CS8618
+    private IDisposable _disposable;
+    private IMapper _mapper;
+#pragma warning restore CS8618
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      var serviceScope = new ServiceCollection().AddMapping().BuildServiceProvider().CreateScope();
+
+      _disposable = serviceScope;
+      _mapper = serviceScope.ServiceProvider.GetRequiredService<IMapper>();
+    }
+
+    [TestCleanup]
+    public void CleanUp()
+    {
+      _disposable?.Dispose();
+    }
+  }
+}
