051647ac3291d13dce5afa92ac5642d4ee23ab4e
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-09-08 10:52:01 +0300

    add json converter for search todo list tasks

---
+++ AspNetRestApiSample.Api/Serialization/SearchTodoListTasksRecordResponseDtoBaseJsonConverter.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetRestApiSample.Api.Serialization
+{
+  using System.Text.Json;
+  using System.Text.Json.Serialization;
+
+  using AspNetRestApiSample.Api.Dtos;
+
+  public sealed class SearchTodoListTasksRecordResponseDtoBaseJsonConverter
+    : JsonConverter<SearchTodoListTasksRecordResponseDtoBase>
+  {
+    public override SearchTodoListTasksRecordResponseDtoBase? Read(
+      ref Utf8JsonReader reader,
+      Type typeToConvert,
+      JsonSerializerOptions options)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void Write(
+      Utf8JsonWriter writer,
+      SearchTodoListTasksRecordResponseDtoBase value,
+      JsonSerializerOptions options)
+    {
+      if (value == null)
+      {
+        writer.WriteNullValue();
+      }
+      else
+      {
+        JsonSerializer.Serialize(writer, value, value.GetType(), options);
+      }
+    }
+  }
+}
