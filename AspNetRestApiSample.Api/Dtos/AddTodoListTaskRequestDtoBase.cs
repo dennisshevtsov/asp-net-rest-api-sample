d34fa137aa0db10a87881a64f981b852257c54ee
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-09-09 11:48:32 +0300

    add type to add todo list task dto

--- AspNetRestApiSample.Api/Dtos/AddTodoListTaskRequestDtoBase.cs
+++ AspNetRestApiSample.Api/Dtos/AddTodoListTaskRequestDtoBase.cs
@@ -6,6 +6,16 @@ namespace AspNetRestApiSample.Api.Dtos
 {
   using AspNetRestApiSample.Api.Indentities;
 
+  /// <summary>Represents types of a TODO list task.</summary>
+  public enum TodoListTaskType : byte
+  {
+    /// <summary>A value that indicates if a TODO list task is a day task.</summary>
+    Day = 0,
+
+    /// <summary>A value that indicates if a TODO list task is a period task.</summary>
+    Period = 1,
+  }
+
   /// <summary>Represents data to add a task to a todo list.</summary>
   public abstract class AddTodoListTaskRequestDtoBase : ITodoListIdentity
   {
@@ -17,5 +27,8 @@ namespace AspNetRestApiSample.Api.Dtos
 
     /// <summary>Gets/sets an object that represents a description of a todo list task.</summary>
     public string? Description { get; set; }
+
+    /// <summary>Gets/sets an object that represents a type of a TODO list task.</summary>
+    public TodoListTaskType Type { get; set; }
   }
 }
