9275fb4f4f2c4da79679fd22d3a2b66c80af2276
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-09-13 14:43:47 +0300

    clean code

--- AspNetRestApiSample.Api/Serialization/TodoListTaskWriterJsonConverter.cs
+++ AspNetRestApiSample.Api/Serialization/TodoListTaskWriterJsonConverter.cs
@@ -1,4 +1,8 @@
-﻿namespace AspNetRestApiSample.Api.Serialization
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetRestApiSample.Api.Serialization
 {
   using System.Text.Json;
   using System.Text.Json.Serialization;
