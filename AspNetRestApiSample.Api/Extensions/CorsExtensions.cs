6daa11c8c0823c56d7aac91325a703fc677f19b1
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-10-19 12:06:44 +0300

    config cors through ext

---
+++ AspNetRestApiSample.Api/Extensions/CorsExtensions.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+  public static class CorsExtensions
+  {
+    public static IServiceCollection AddCofiguredCors(
+      this IServiceCollection services, IConfiguration configuration)
+    {
+      var settings = configuration.Get<CorsSettings>();
+
+      services.AddCors(options =>
+      {
+        options.AddDefaultPolicy(policy =>
+        {
+          for (int i = 0; i < settings.OriginCollection.Length; i++)
+          {
+            policy.WithMethods(settings.OriginCollection[i]);
+          }
+
+          for (int i = 0; i < settings.MethodCollection.Length; i++)
+          {
+            policy.WithMethods(settings.MethodCollection[i]);
+          }
+
+          for (int i = 0; i < settings.HeaderCollection.Length; i++)
+          {
+            policy.WithMethods(settings.HeaderCollection[i]);
+          }
+        });
+      });
+
+      return services;
+    }
+
+    public sealed class CorsSettings
+    {
+      public string? Origins { get; set; }
+
+      public string[] OriginCollection => CorsSettings.ToCollection(Origins);
+
+      public string? Methods { get; set; }
+
+      public string[] MethodCollection => CorsSettings.ToCollection(Methods);
+
+      public string? Headers { get; set; }
+
+      public string[] HeaderCollection => CorsSettings.ToCollection(Headers);
+
+      public static string[] ToCollection(string? value)
+      {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+          return new string[0];
+        }
+
+        return value.Split(new[] { '\u002C' }, StringSplitOptions.RemoveEmptyEntries);
+      }
+    }
+  }
+}
--- AspNetRestApiSample.Api/Program.cs
+++ AspNetRestApiSample.Api/Program.cs
@@ -2,22 +2,11 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
-using Microsoft.Net.Http.Headers;
-
 using AspNetRestApiSample.Api.Binding;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-                {
-                  options.AddDefaultPolicy(policy => policy.WithOrigins("http://localhost:4200")
-                                                           .WithMethods(HttpMethods.Options,
-                                                                        HttpMethods.Get,
-                                                                        HttpMethods.Post,
-                                                                        HttpMethods.Put,
-                                                                        HttpMethods.Delete)
-                                                           .WithHeaders(HeaderNames.ContentType));
-                });
+builder.Services.AddCofiguredCors(builder.Configuration);
 builder.Services.AddControllers(options => options.ModelBinderProviders.Insert(0, new RequestDtoBinderProvider(options)))
                 .AddJsonSerialization();
 builder.Services.AddSwaggerGen(options =>
